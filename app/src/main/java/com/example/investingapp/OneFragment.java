482b767e70f3caf03816521507e2769d4a9dee03
BALU <Balubharath.a1@gmail.com>
2020-01-03 13:48:41 +0530

    updating

--- app/src/main/java/com/example/investingapp/OneFragment.java
+++ app/src/main/java/com/example/investingapp/OneFragment.java
@@ -4,17 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 class OneFragment extends Fragment {
+
+    ListView listView;
+    String indicesarraynames[]={"Nifty Bank","Nifty 50","SNAP" };
+    String curentvalues[]={"501664","412583","145244"};
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view=inflater.inflate(R.layout.one,container,false);
+
+
+
+
+
+
+
+
+
+
+
+
         return view;
     }
 }
